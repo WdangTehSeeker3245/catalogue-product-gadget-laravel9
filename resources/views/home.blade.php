deb7395a3f2792ab71c20e8845f2a329072572a1
WdangTehSeeker3245 <faizalnf3245@gmail.com>
2023-09-22 21:45:14 +0700

    first commit, design home ui

---
+++ resources/views/home.blade.php
@@ -0,0 +1,272 @@
+@include('layout/header')
+@include('layout/navbar')
+    {{-- Header Hero --}}
+    <div class="container-header">
+        <div class="header-desktop">
+            <div class="row">
+                <div class="col-8">
+                    <h1 class="text-uppercase fw-bold">We provide product list <br> information,reviews and ratings</h1>
+                    <a href="#" class="btn btn-info text-uppercase fw-bold text-light mt-4 px-3 py-3 shadow">View All Product</a>
+                </div>
+                <div class="col"><img class="img-header shadow" src="https://static.republika.co.id/uploads/images/inpicture_slide/gadget-_140319092257-545.jpg" alt="Header Image"></div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    {{-- Card Top Product --}}
+    <div class="card mx-4 shadow mb-4">
+        <div class="card-body">
+            <div class="card-title">
+                <div class="row">
+                    <div class="col"><h3 class="text-uppercase fw-bold">Top Product</h3></div>
+                    <div class="col"> <a href="#" class="btn btn-info float-end text-uppercase fw-bold text-light">View All Top Product <i class="fa-solid fa-arrow-right"></i></a></div>
+                </div>
+            </div>
+            <hr>
+            <div class="row row-cols-1 row-cols-lg-4">
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://carisinyal.com/wp-content/uploads/2022/07/poco-c40_.webp" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Xiaomi Poco C40</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2023/7/12/3687e466-1f41-488a-88c7-bd51038cc7b1.png" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Itel S23</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2023/5/11/fa63a92d-97ee-4ccd-b924-dcee17202f93.jpg" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Realme C33</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/11/4/f82619ad-8e08-46e2-b73f-1b4dd4a490fe.jpg" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Samsung Galaxy A04</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Card Top Product --}}
+    <div class="card mx-4 shadow mb-4">
+        <div class="card-body">
+            <div class="card-title">
+                <div class="row">
+                    <div class="col"><h3 class="text-uppercase fw-bold">Top Product Smartphone</h3></div>
+                    <div class="col"> <a href="#" class="btn btn-info float-end text-uppercase fw-bold text-light">View All Top Product <i class="fa-solid fa-arrow-right"></i></a></div>
+                </div>
+            </div>
+            <hr>
+            <div class="row row-cols-1 row-cols-lg-4">
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://carisinyal.com/wp-content/uploads/2022/07/poco-c40_.webp" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Xiaomi Poco C40</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2023/7/12/3687e466-1f41-488a-88c7-bd51038cc7b1.png" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Itel S23</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2023/5/11/fa63a92d-97ee-4ccd-b924-dcee17202f93.jpg" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Realme C33</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col">
+                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/11/4/f82619ad-8e08-46e2-b73f-1b4dd4a490fe.jpg" class="card-img-top rounded-0" alt="...">
+                            <div class="card-body mt-3 mb-3">
+                              <div class="row">
+                                <div class="col-10">
+                                  <h4 class="card-title">Samsung Galaxy A04</h4>
+                                  <p class="card-text">
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star-fill text-warning"></i>
+                                    <i class="bi bi-star"></i>
+                                    (123)
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row align-items-center text-center g-0">
+                              <div class="col-4">
+                                <h5>Rp. 1,3 Jt</h5>
+                              </div>
+                              <div class="col-8">
+                                <a href="#" class="btn btn-dark w-100 p-3 rounded-0 text-warning">View Detail</a>
+                              </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@include('layout/footer')    
